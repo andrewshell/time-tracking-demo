12d2b98e4af867f9ae49584e4b63a37b4924cca9
Andrew Shell <andrew@andrewshell.org>
2020-01-07 11:43:02 -0600

    Updates

---
+++ precache-manifest.ec4c870a27b90916533bf2d7dc417087.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3092a87997d1bdad0010312a1dfe3f3f",
+    "url": "/time-tracking-demo/index.html"
+  },
+  {
+    "revision": "db63b0a8bac3bcfae5aa",
+    "url": "/time-tracking-demo/static/css/main.3fa08c2a.chunk.css"
+  },
+  {
+    "revision": "5468cfcde2fc11e349cf",
+    "url": "/time-tracking-demo/static/js/2.36b7aa5c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/time-tracking-demo/static/js/2.36b7aa5c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "db63b0a8bac3bcfae5aa",
+    "url": "/time-tracking-demo/static/js/main.9263c542.chunk.js"
+  },
+  {
+    "revision": "11dbfa79576cecf53189",
+    "url": "/time-tracking-demo/static/js/runtime-main.c052417f.js"
+  }
+]);
